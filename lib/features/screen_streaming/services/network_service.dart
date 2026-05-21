2d22cc4f68965cf9999540d2962ae3d6da323583
Leta-D <letadeju.1278@gmail.com>
2026-05-21 10:00:00 +0300

    some changes

--- lib/features/screen_streaming/services/network_service.dart
+++ lib/features/screen_streaming/services/network_service.dart
@@ -3,8 +3,14 @@ import 'package:peer_view_2/features/screen_streaming/models/host_network_info.d
 /// Provides host network information for client connection URLs.
 abstract interface class NetworkService {
   /// Reads the current local IPv4 address and builds the WebSocket URL.
-  Future<HostNetworkInfo> getHostNetworkInfo({int port = 8080});
+  Future<HostNetworkInfo> getHostNetworkInfo({
+    int port = 8080,
+    String webSocketPath = '/stream',
+  });
 
   /// Emits updated network information when connectivity changes.
-  Stream<HostNetworkInfo> watchNetworkInfo({int port = 8080});
+  Stream<HostNetworkInfo> watchNetworkInfo({
+    int port = 8080,
+    String webSocketPath = '/stream',
+  });
 }
