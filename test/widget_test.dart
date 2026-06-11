ff5e5082ee0b2b25073b14626381d5e0fbdf8961
Leta-D <letadeju.1278@gmail.com>
2026-06-11 10:00:00 +0300

    app test function improved

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:peer_view_2/presentation/main_screens/role_selection_screen.dart';
+import 'package:peer_view_2/constants/app_theme.dart';
 import 'package:peer_view_2/core/di/injection.dart';
+import 'package:peer_view_2/presentation/main_screens/role_selection_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  setUp(configureDependencies);
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    await configureDependencies();
+  });
 
-  testWidgets('Role selection screen shows host and client options', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const MaterialApp(home: RoleSelectionScreen()));
+  testWidgets('Role selection screen shows host and client options', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        theme: AppTheme.dark,
+        home: const RoleSelectionScreen(),
+      ),
+    );
 
     expect(find.text('Host'), findsOneWidget);
     expect(find.text('Client'), findsOneWidget);
-    expect(find.text('Choose a role'), findsOneWidget);
+    expect(find.textContaining('Choose'), findsOneWidget);
   });
 }
